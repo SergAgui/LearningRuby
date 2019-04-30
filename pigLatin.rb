24a95601e221ee4b85ec8477915c2be5bbcb22d2
Sergio <Sergeagui@gmail.com>
2019-04-30 14:28:41 -0500

    pig latin progress, but comming to a stopping point

--- pigLatin.rb
+++ pigLatin.rb
@@ -1,8 +1,21 @@
-sentance = "This is different" #gets.chomp.downcase
-only_words = sentance.split(" ")
-only_words.each do |words|
-    part = words.split(/([aeoiu].*)/)
-    finished_word = "#{part[1]}#{part[0]}ay "
-end
-puts "#{only_words}"
+puts "Please enter a few words or a sentance: "
 
+input = "Different than before" #gets.chomp.downcase
+
+# Translate one word to pig latin
+def translate_word(word)
+    @part = word.split(/([aeoiu].*)/)
+    return "#{@part[1]}#{@part[0].downcase}ay"
+end
+#breakpoint puts translate_word(input)
+
+# Translate several words in a string by translating them individually first
+def translate_sentance(sentance)
+    @only_words = sentance.split(" ")
+    @only_words.length.times do |word|
+        @translated[word] = translate_word(@only_words[word])
+    end
+    #breakpoint puts @only_words[word]
+    return @translated.join(" ")
+end
+puts translate_sentance(input)
